e0daa53023e4f2fe7170d9cabaed0f0a682cab27
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-01-26 17:05:16 +0530

    Added material design with NoAnimationModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { CustomMaterialModule } from './custom-material/custom-material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule,
+    NoopAnimationsModule // initally disabled
   ],
   providers: [],
   bootstrap: [AppComponent]
