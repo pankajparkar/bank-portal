6bbe8da4d6881ee71f55bed5d29ddfeb7fefc64b
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-01-29 01:14:06 +0530

    Removed unsed imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
