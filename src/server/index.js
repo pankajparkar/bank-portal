a28aac285ddc2b0fd67584f1d5c6b84272e4e8a4
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-02-01 02:04:54 +0530

    Added compression and pathlocation strategy support.

--- src/server/index.js
+++ src/server/index.js
@@ -2,40 +2,22 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var app = express();
-// var favicon = require('serve-favicon');
-var port = process.env.PORT || 4200;
+const compression = require('compression');
+const port = process.env.PORT || 8080;
 
-// app.use(favicon(__dirname + '/favicon.ico'));
+app.use(compression({ threshold: 0 }));
 
 app.get('/ping', function (req, res, next) {
   console.log(req.body);
   res.send('pong');
 });
 
-
 var staticRoot = __dirname;
 
 app.use(express.static(staticRoot));
 
-// app.use(express.static('./'));
-// Any deep link calls should return index.html
-// app.use('/*', express.static('./index.html'));
-
-app.use(function (req, res, next) {
-
-  // if the request is not html then move along
-  var accept = req.accepts('html', 'json', 'xml');
-  if (accept !== 'html') {
-    return next();
-  }
-
-  // // if the request has a '.' assume that it's for a file, move along
-  // var ext = path.extname(req.path);
-  // if (ext !== '') {
-  //   return next();
-  // }
-
-  fs.createReadStream(staticRoot + 'index.html').pipe(res);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(port, function () {
